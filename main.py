f171b278124dd52838273c555638eecbd92c782a
mickyhsiao03 <52259287+mickyhsiao03@users.noreply.github.com>
2021-11-30 10:45:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -405,7 +405,7 @@ def main():     #main function, maps out a route for drone to start mission
     
     now = datetime.now()
     curTime = now.strftime("%H%M%S")
-    with open(f"C:/Command.txt", "w") as f: #writes tello command file with time stamp. command.txt official file name
+    with open(f"C:/Users/Micky/Desktop/Command.txt", "w") as f: #writes tello command file with time stamp. command.txt official file name
         f.write('command')
         f.write('\n')
         f.write('delay 2')
